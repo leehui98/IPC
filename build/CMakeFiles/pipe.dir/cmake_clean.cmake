256e42b72fd001349abf9645ffd888a2ef9e5345
leehui <leehui98@163.com>
2021-01-18 21:05:22 +0800

    add pipe

---
+++ build/CMakeFiles/pipe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pipe.dir/pipe.cpp.o"
+  "pipe"
+  "pipe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pipe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- pipe.cpp
+++ pipe.cpp
@@ -1,2 +1,24 @@
 #include<iostream>
 #include<unistd.h>
+using namespace std;
+int main(){
+    int pid;
+    int fd[2];
+    if(pipe(fd)<0){
+        cout<<"error in pipe()"<<endl;
+        exit(1);
+    }
+    if((pid=fork())<0){
+        cout<<"error in fork"<<endl;
+        exit(1);
+    }else if(pid>0){//父进程
+        close(fd[0]);
+        write(fd[1],"hello\n",sizeof("hello\n"));
+    }else if(pid==0){//子进程
+        close(fd[1]);
+        char buff[100]{};
+        read(fd[0],buff,sizeof(buff));
+        cout<<buff;
+    }
+    return 0;
+}
